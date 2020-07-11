472de1b1ec9c3c6769bbdb48375875ca90fe8e5a
Suhair Zain <suhair@languagezen.com>
2020-07-11 14:46:56 +0530

    Rename build dir to dist
    Run build before versioning
    
    Remove deleted build dir from gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: 'production',
     entry: './src/index.ts',
     output: {
-        path: path.resolve('build'),
+        path: path.resolve('dist'),
         filename: 'index.js',
         libraryTarget: 'commonjs2',
     },
